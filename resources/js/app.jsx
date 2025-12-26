a69810d2fc592dcc293871e1bbb19f59577cd782
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-26 14:34:16 +0800

    * changed progress bar color

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,7 +8,7 @@ import { createRoot } from 'react-dom/client';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} | ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
@@ -20,6 +20,6 @@ createInertiaApp({
         root.render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: '#00c853',
     },
 });
