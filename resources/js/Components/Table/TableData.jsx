cb8e07412e698b306aba28257b572dc8f5d9116f
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-01-27 15:01:32 +0800

    remove truncate from the TableData.jsx

--- resources/js/Components/Table/TableData.jsx
+++ resources/js/Components/Table/TableData.jsx
@@ -1,10 +1,10 @@
 export default function TableData({ children, colSpan, customWidth = "", className = "" }) {
-    let baseClassName = "px-4 py-3 text-sm dark:text-white font-semibold truncate";
+    let baseClassName = "px-4 py-3 text-sm dark:text-white font-semibold";
 
     if (customWidth === "") {
-        baseClassName = "min-w-[150px] max-w-[150px] px-2 py-3 text-sm dark:text-white font-semibold truncate";
+        baseClassName = "min-w-[150px] max-w-[150px] px-2 py-3 text-sm dark:text-white font-semibold";
     } else {
-        baseClassName = "px-4 py-3 text-sm dark:text-white font-semibold truncate";
+        baseClassName = "px-4 py-3 text-sm dark:text-white font-semibold";
     }
 
     return (
